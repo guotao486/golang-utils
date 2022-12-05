3ad5f8b7e52acb7b32f63d76b88bdeeb227cb233
793954500@qq.com <793954500@qq.com>
2022-12-05 09:46:58 +0800

    feat: first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+/*
+ * @Author: GG
+ * @Date: 2022-12-05 09:39:10
+ * @LastEditTime: 2022-12-05 09:44:43
+ * @LastEditors: GG
+ * @Description:
+ * @FilePath: \golang-utils\main.go
+ *
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("frist commit")
+}
